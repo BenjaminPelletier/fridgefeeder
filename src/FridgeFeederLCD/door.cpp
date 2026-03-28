42495456cc60240c59f3f749e713d883ea95cae6
Benjamin Pelletier <pelletier@alumni.caltech.edu>
2026-03-28 15:38:06 -0700

    Only command servo when changing value

--- src/FridgeFeederLCD/door.cpp
+++ src/FridgeFeederLCD/door.cpp
@@ -7,7 +7,10 @@
 const unsigned char PIN_SERVO = A5;
 int door_value;
 unsigned long t_servo_last = 0;
+unsigned long servo_last = 0;
+unsigned long servo_pulses = 0;
 const int SERVO_MS = 20;
+const int SERVO_CHANGE_PULSES = 100;
 
 void init_door() {
   pinMode(PIN_SERVO, OUTPUT);
@@ -29,9 +32,17 @@ void set_door(int value) {
 void update_door() {
   unsigned long t1 = millis();
   if (t1 > t_servo_last + SERVO_MS) {
-    digitalWrite(PIN_SERVO, HIGH);
-    delayMicroseconds(door_value);
-    digitalWrite(PIN_SERVO, LOW);
+    if (door_value == servo_last) {
+      servo_pulses++;
+    } else {
+      servo_pulses = 0;
+      servo_last = door_value;
+    }
+    if (servo_pulses < SERVO_CHANGE_PULSES) {
+      digitalWrite(PIN_SERVO, HIGH);
+      delayMicroseconds(door_value);
+      digitalWrite(PIN_SERVO, LOW);
+    }
     t_servo_last = t1;
   }
 }
